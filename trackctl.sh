46bd5a24ec783d25a7d7a30551fea73b678d5650
Kirill Spitsyn <ksp@google.com>
2021-03-04 16:48:17 -0800

    Change the project's path.

--- trackctl.sh
+++ trackctl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/home/ksp/mfiles/projects/trackd/trackd-venv/bin/python \
-    /home/ksp/mfiles/projects/trackd/trackctl.py \
+/home/ksp/projects/trackd/trackd-venv/bin/python \
+    /home/ksp/projects/trackd/trackctl.py \
     $@
