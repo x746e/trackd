31aa3480a5200eacf4ab9b2bb4c147b6c79b9a9b
Kirill Spitsyn <ksp@google.com>
2021-03-07 07:44:52 -0800

    Checkpointing before removing grpc-web.
    Javascript implementation of protobuf uses eval (https://github.com/protocolbuffers/protobuf/issues/7778), which isn't allowed in Chrome extensions v3 manifest.  But we need v3 manifest to use `chrome.tabGroups` API, so I don't see an easy way to use grpc from a Chrome plugin.  And, at the end of the day, we don't really gain that much from grpc in that small kind of project.

--- chrome_extension/client_chrome.js
+++ chrome_extension/client_chrome.js
@@ -4,6 +4,44 @@ chrome.runtime.onInstalled.addListener(function() {
     const {SessionChangedRequest} = require('./chrome_pb.js');
     const {ChromeClient} = require('./chrome_grpc_web_pb.js');
 
+    function onFocusChanged(windowId) {
+        console.log(`${windowId} is focused`);
+
+        let groupId = windowToSessionMap.get(windowId);
+        chrome.tabGroups.get(groupId, group => {
+            console.log(`groupId: ${groupId}, group: ${group}`);
+        });
+    }
+
+    chrome.windows.onFocusChanged.addListener(onFocusChanged);
+
+    let windowToSessionMap = new Map();
+    windowToSessionMap.set(-1, -1);
+
+    chrome.windows.getAll({populate: true}, windows => {
+        console.log(windows);
+
+        windowToSessionMap.clear();
+
+        for (let window_ of windows) {
+            for (let tab of window_.tabs) {
+                if (tab.groupId != -1) {
+                    windowToSessionMap.set(window_.id, tab.groupId);
+                    break;
+                }
+            }
+            windowToSessionMap.set(window_.id, -2);
+        }
+
+        // The assumption is that as we just started, the Chrome should still be focused.
+        // It may be good to check in trackd though that Chrome's XWindow is indeed focused
+        // when this plugin reports so.
+        windows.getLastFocused({}, window_ => {
+            onFocusChanged(window_.id);
+        });
+    });
+
+
     let client = new ChromeClient('http://localhost:3142', null, null);
 
     let request = new SessionChangedRequest();
